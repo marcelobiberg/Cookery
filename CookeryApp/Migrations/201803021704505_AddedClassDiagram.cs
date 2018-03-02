084c0e4e8444f2a2d575e1e2ed9e93b77cfeaf28
Marcelo Biberg <biberg.marcelo@gmail.com>
2018-03-02 14:05:21 -0300

    AddedClassDiagram

---
+++ CookeryApp/Migrations/201803021704505_AddedClassDiagram.cs
@@ -0,0 +1,16 @@
+namespace CookeryApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedClassDiagram : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
